e7509beed3155dbc414b78f7e590270b3c407d06
SubDeath <ramiroruggieri@gmail.com>
2026-04-05 21:15:10 -0300

    Agrego vista de Hola Mundo

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserView, RoleView
+from .views import UserView, RoleView, hola_mundo
 
 urlpatterns = [
+    path('', hola_mundo),  # 👈 esto muestra Hola Mundo
     path('users/', UserView.as_view()),
     path('roles/', RoleView.as_view()),
 ]
